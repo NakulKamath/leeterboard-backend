815404cc7db1af20a8647ae5c476017fbfb863f1
Nakul Kamath <69424522+NakulKamath@users.noreply.github.com>
2025-06-05 22:55:41 +0530

    FireStore Setup

--- src/app.ts
+++ src/app.ts
@@ -271,4 +271,26 @@ app.get('/:username/submission', leetcode.submission);
 app.get('/:username/acSubmission', leetcode.acSubmission);
 app.get('/:username/calendar', leetcode.calendar);
 
+const {initializeApp} = require('firebase/app');
+const { 
+  getFirestore, 
+  getDoc,
+  doc,
+  updateDoc,
+  setDoc,
+} = require('firebase/firestore');
+require('dotenv').config();
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
+
 export default app;
