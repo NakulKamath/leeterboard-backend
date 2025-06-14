43dc0f3fbd7fab7b6dd61e76a6792adadf171795
Nakul Kamath <69424522+NakulKamath@users.noreply.github.com>
2025-06-14 21:53:41 +0530

    Internal server changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import app from './app';
-import config from './config';
 
-app.listen(config.port, () => {
-  console.log(`Server is running at => http://localhost:${config.port} ⚙️`);
+app.listen(3000, '127.0.0.1', () => {
+  console.log('Server running on http://127.0.0.1:3000');
 });
